b2f12741baee62f375cd61f5ff22dde64ee455d2
bolshakovk <bolshakovk1234@gmail.com>
2020-12-08 12:20:30 +0300

    test commit((added bd config)

--- config.py
+++ config.py
@@ -1,4 +1,20 @@
 import os
+from flask import Flask
+from flask_sqlalchemy.utils import sqlalchemy_version
+import sqlalchemy
+from sqlalchemy.engine import create_engine
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from app import routes, models
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or '12345'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('postgresql+psycopg2://root:pass@localhost/my_db') or \
+        'postgresql:///' + os.path.join(basedir, 'app.db')
