84f50264a8afb589c869163a73353d57e51eb203
bolshakovk <bolshakovk1234@gmail.com>
2020-12-11 14:33:42 +0300

    modified config and added docker

--- app/models.py
+++ app/models.py
@@ -1,49 +1,43 @@
 class User:
-    id = 1
+    id = None
     email = "test@test.com"
     password = "qwerty123"
     phone = "891012345676"
-
-class Manager(User):
-    pass
-
 class Client(User):
-    id = 1
+    id = None
     email = "test@test.test"
     password = "qwerty123"
     phone = "87035568730"
-
-class PhotoSalon(Order):
-    id = 1
-    address = "Пушкина дом Колотушкина"
-    
 class Order(Client):
-    id = 1
-    client_id = 1
+    id = None
+    client_id = None
     source = "source"
-    photo_salon_id = 1
-    status = 1
+    photo_salon_id = None
+    status = None
+class PhotoSalon(Order):
+    id = None
+    address = "Пушкина дом Колотушкина"
 
 class Order_Service(Order):
-    id = 1 
-    order_id = 1
-    service_id = 1
+    id = None 
+    order_id = None
+    service_id = None
 
-class Service(Order-Service):
-    id = 1
+class Service(Order_Service):
+    id = None
     tittle = "test"
     decription = "Описание"
-    price = 1000
+    price = None
 
 class Employee(PhotoSalon):
-    photo_salon_id = 1
+    photo_salon_id = None
 
 class PhotoSalon_Equipment(PhotoSalon):
-    id = 1
-    photo_salon_id = 1
-    equipment_id = 1
+    id = None
+    photo_salon_id = None
+    equipment_id = None
 
 class Equipment(PhotoSalon_Equipment):
-    id = 1
+    id = None
     tittle = "tittle"
     type = "type"
