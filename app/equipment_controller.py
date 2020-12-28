4a680569e31879f43649495ad02233a74dd73100
eltio <i.n.1998@inbox.ru>
2020-12-29 00:54:48 +0300

    Allow access to equipments to managers

--- app/equipment_controller.py
+++ app/equipment_controller.py
@@ -2,7 +2,7 @@ from flask import jsonify, request, make_response
 from flask.views import MethodView
 
 
-from .models import Equipment
+from .models import Equipment, UserType
 from app import auth, equipment_repository
 
 
@@ -11,12 +11,18 @@ class EquipmentsController(MethodView):
     @auth.login_required
     def get():
         """Запрос инфы о оборудовании"""
+        if auth.current_user().type != UserType.manager:
+            return make_response(jsonify(msg='restricted'), 403)
+
         return jsonify(equipments=equipment_repository.get_all())
 
     @staticmethod
     @auth.login_required
     def post():
         """Добавление нового девайса в систему"""
+        if auth.current_user().type != UserType.manager:
+            return make_response(jsonify(msg='restricted'), 403)
+
         data = request.form
         try:
             id = equipment_repository.create(data)
@@ -30,16 +36,25 @@ class EquipmentItemController(MethodView):
     @auth.login_required
     def get(store_id, entity_id):
         """Инфо о конкретном оборудовании в конкретном салоне (уровень запраки принтера или типа того)"""
+        if auth.current_user().type != UserType.manager:
+            return make_response(jsonify(msg='restricted'), 403)
+
         return jsonify(store_id=store_id, **Equipment.mock(entity_id))
 
     @staticmethod
     @auth.login_required
     def post(store_id, entity_id):
         """Присвоить оборудование с заданным id фотосалону"""
+        if auth.current_user().type != UserType.manager:
+            return make_response(jsonify(msg='restricted'), 403)
+
         return make_response(jsonify({"store_id": store_id, "id": entity_id, "msg": "equipment is changed"}), 200)
 
     @staticmethod
     @auth.login_required
     def delete(store_id, entity_id):
         """Забрать оборудование у фотосалона"""
+        if auth.current_user().type != UserType.manager:
+            return make_response(jsonify(msg='restricted'), 403)
+        
         return make_response(jsonify({"store_id": store_id, "id": entity_id, "msg": "equipment is removed"}), 200)
